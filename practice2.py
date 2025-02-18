0ca10cddcc665fd569a2c2018f3e018be40d4787
Aryanmahakur <mahakuraryan@gmail.com>
2025-02-18 23:12:12 +0530

    uf

--- practice2.py
+++ practice2.py
@@ -14,17 +14,43 @@
 #     a +=1
 #3
 #4
-a=0
-min=1
-Input=[10, 20, 5, 8, 50]
-print(min(Input))
-print(max(Input))
-while a<len(Input):
-    print(Input[a])
-    a+=1    
-if min>input[a]:
-    min =input[a]
-    print[a]
+
+# a = 0
+# Input = [10, 20, 5, 8, 50]
+
+# # Initialize max_val and second_max_val
+# max_val = float('-inf')
+# second_max_val = float('-inf')
+
+# while a < len(Input):
+#     print(Input[a])
+#     if Input[a] > max_val:
+#         second_max_val = max_val
+#         max_val = Input[a]
+#     elif Input[a] > second_max_val and Input[a] != max_val:
+#         second_max_val = Input[a]
+#     a += 1
+
+# print(f"The second maximum value is {second_max_val}")
+
+# num = 5
+# factorial = 1
+
+# while num > 0:
+#     factorial *= num
+#     num -= 1
+
+# print(f"The factorial of 5 is {factorial}")
    
 
+    
+lists = [10, 50, 40, 30, 80]
+max_val = lists[0]
+a = 0
 
+while a < len(lists):
+    if max_val < lists[a]:
+        max_val = lists[a]
+    a += 1
+
+print("max value is", max_val)
