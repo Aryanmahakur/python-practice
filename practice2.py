0c9b0e072afb6af3733487cf6cb4bc968600c48a
Aryanmahakur <mahakuraryan@gmail.com>
2025-02-18 22:34:03 +0530

    uf

---
+++ practice2.py
@@ -0,0 +1,30 @@
+#q1
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# height = float(input("Enter your height in meters: "))
+# weight = float(input("Enter your weight in kilograms: "))
+
+# print(f"Name: {name} Age: {age} Height: {height} Weight: {weight}")
+
+#q2
+# a=0
+# while a<=100:
+#     if(a%3==0 and a%5==0):
+#      print(a)
+#     a +=1
+#3
+#4
+a=0
+min=1
+Input=[10, 20, 5, 8, 50]
+print(min(Input))
+print(max(Input))
+while a<len(Input):
+    print(Input[a])
+    a+=1    
+if min>input[a]:
+    min =input[a]
+    print[a]
+   
+
+
