441cc4df4d7f73272ac3d59fbdf2a82e87c7ea44
Aryanmahakur <mahakuraryan@gmail.com>
2025-02-22 22:54:19 +0530

    uf

--- practice3.py
+++ practice3.py
@@ -1,32 +1,32 @@
-import re
+# import re
 
-def is_strong_password(password):
-    # Check if the password is at least 8 characters long
-    if len(password) < 8:
-        return False
+# def is_strong_password(password):
+#     # Check if the password is at least 8 characters long
+#     if len(password) < 8:
+#         return False
     
-    # Check if the password contains at least one uppercase letter
-    if not re.search(r'[A-Z]', password):
-        return False
+#     # Check if the password contains at least one uppercase letter
+#     if not re.search(r'[A-Z]', password):
+#         return False
     
-    # Check if the password contains at least one lowercase letter
-    if not re.search(r'[a-z]', password):
-        return False
+#     # Check if the password contains at least one lowercase letter
+#     if not re.search(r'[a-z]', password):
+#         return False
     
-    # Check if the password contains at least one digit
-    if not re.search(r'[0-9]', password):
-        return False
+#     # Check if the password contains at least one digit
+#     if not re.search(r'[0-9]', password):
+#         return False
     
-    # Check if the password contains at least one special character
-    if not re.search(r'[!@#$%^&*]', password):
-        return False
+#     # Check if the password contains at least one special character
+#     if not re.search(r'[!@#$%^&*]', password):
+#         return False
     
-    return True
+#     return True
 
-# Example usage
-print(is_strong_password("Password123!"))  # This will print: True
-print(is_strong_password("weakpass"))      # This will print: False # This will print: False
-a
-b
-c
-d
+# # Example usage
+# print(is_strong_password("Password123!"))  # This will print: True
+# print(is_strong_password("weakpass"))      # This will print: False # This will print: False
+#
+s = "aryan"
+reversed_s = s[::-1]
+print(reversed_s)  # This will print: "nayra" # This will print: "nayra"
